d449c93dcc9cb109fba9206597365267db1859ac
BauwenDR <BauwenDR@gmail.com>
2026-02-11 16:06:42 +0100

    Flee behaviour (except for debug lines)

--- Source/GameAIProg/Movement/SteeringBehaviors/Steering/SteeringBehaviors.cpp
+++ Source/GameAIProg/Movement/SteeringBehaviors/Steering/SteeringBehaviors.cpp
@@ -10,8 +10,16 @@ SteeringOutput Seek::CalculateSteering(float DeltaT, ASteeringAgent& Agent)
 
 	Steering.LinearVelocity = Target.Position - Agent.GetPosition();
 
-	const FVector2D DebugTarget = Agent.GetPosition() + Steering.LinearVelocity / Steering.LinearVelocity.Length() * Agent.GetLinearVelocity().Length();
-	DrawDebugLine(Agent.GetWorld(), FVector(Agent.GetPosition(), 0.0f), FVector(DebugTarget, 0.0f), FColor::Magenta);	// will be handy
+	// const FVector2D DebugTarget = Agent.GetPosition() + Steering.LinearVelocity / Steering.LinearVelocity.Length() * Agent.GetLinearVelocity().Length() * DeltaT;
+	// DrawDebugLine(Agent.GetWorld(), FVector(Agent.GetPosition(), 0.0f), FVector(DebugTarget, 0.0f), FColor::Magenta);	// will be handy
+
+	return Steering;
+}
+
+SteeringOutput Flee::CalculateSteering(float DeltaT, ASteeringAgent& Agent)
+{
+	SteeringOutput Steering = Seek::CalculateSteering(DeltaT, Agent);
+	Steering.LinearVelocity = -Steering.LinearVelocity;
 
 	return Steering;
 }
